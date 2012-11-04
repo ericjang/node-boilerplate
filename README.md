c23a4efdc6cfe486e1ca53303a46f19765efbbd5
Eric Jang <ericjang2004@gmail.com>
2012-11-04 12:54:59 -0500

    upgraded to Express 3.x, using CDNs for JS libraries, added Backbone.js

--- README.md
+++ README.md
@@ -1,27 +1,32 @@
-Node Boilerplate Version 2
+Node Boilerplate
 ==========================
-*Requires Node v0.6.6 (or newer)*
-node-boilerplate takes html-boilerplate, express, connect, jade and Socket.IO and organizes them into a ready to use website project. 
+*Requires Node v0.8.6 (or newer)*
+node-boilerplate takes html-boilerplate, express, connect, jade and Socket.IO and organizes them into a ready to use website project. This is intended for my personal NodeJS projects but anybody is more than welcome to fork a copy and run this.
 
 Forked from Robrighter's repo, with my own custom modifications to include:
 
 - Twitter Bootstrap
-- DevNotes.txt (also added to the gitignore)
-- ... and possibly more features to come (everyauth)
+- A DevNotes.txt file (also added to the gitignore)
+- process.ENV detection for heroku apps that bind to MongoDB services (or anything, really...)
+- Backbone.js integration
+- All 3rd party JS libraries are imported using Cloudflare CDN (for less server load)
+- coffee branch with all js files written in coffeescript
+- Uses Express 3.x error handling style for 404 and 500 pages
+- ... more features to come.
 
 To start a project:
 		
 		git clone git://github.com/ericjang/node-boilerplate.git mynewproject
 		cd mynewproject
 		./initproject.sh
-This will copy down all of the boilerplate files, organize them appropriately and init a fresh new git repository within which you can build your next big thing.
 
+This will copy down all of the boilerplate files, organize them appropriately and init a fresh new git repository within which you can build your next big thing.
 
 To run the boilerplate template app:
 
 		node server.js
 
-Go to http://0.0.0.0:8081 and click on the send message link to see socket.io in action.
+Go to http://0.0.0.0:8081 to view your website.
 
 
 Additional Features:
--- templates/app/server.js
+++ templates/app/server.js
@@ -4,46 +4,74 @@ var connect = require('connect')
     , io = require('socket.io')
     , port = (process.env.PORT || 8081);
 
+
+/*
+Set up MongoDB like this for heroku apps
+
+var mongourl = (process.env.NODE_ENV == 'production') ? process.env.MONGOHQ_URL : 'localhost:27017/pulpierfiction_db'
+	,	db = mongo.db(mongourl)
+	, stories = db.collection('stories')
+	, users = db.collection('users');
+
+*/
+
+
+
+/*
+Define your Mongoose Schema here
+
+*/
+
+
 //Setup Express
-var server = express.createServer();
-server.configure(function(){
-    server.set('views', __dirname + '/views');
-    server.set('view options', { layout: false });
-    server.use(connect.bodyParser());
-    server.use(express.cookieParser());
-    server.use(express.session({ secret: "shhhhhhhhh!"}));
-    server.use(connect.static(__dirname + '/static'));
-    server.use(server.router);
+var app = express();
+app.configure(function(){
+    app.set('views', __dirname + '/views');
+    app.set('view options', { layout: false });
+    app.use(connect.bodyParser());
+    app.use(express.cookieParser());
+    app.use(express.session({ secret: "shhhhhhhhh!"}));
+    app.use(connect.static(__dirname + '/static'));
+    app.use(app.router);
+		//error handling compliant with Express 3.x
+		app.use(function(err, req, res, next){
+			console.error(err.stack);
+	    if (err instanceof NotFound) {
+	        res.render('404.jade', { locals: { 
+	                  title : '404 - Not Found'
+	                 ,description: ''
+	                 ,author: ''
+	                 ,analyticssiteid: 'XXXXXXX' 
+	                },status: 404 });
+	    } else {
+	        res.render('500.jade', { locals: { 
+	                  title : 'The Server Encountered an Error'
+	                 ,description: ''
+	                 ,author: ''
+	                 ,analyticssiteid: 'XXXXXXX'
+	                 ,error: err 
+	                },status: 500 });
+	    }
+		});
 });
 
-//setup the errors
-server.error(function(err, req, res, next){
-    if (err instanceof NotFound) {
-        res.render('404.jade', { locals: { 
-                  title : '404 - Not Found'
-                 ,description: ''
-                 ,author: ''
-                 ,analyticssiteid: 'XXXXXXX' 
-                },status: 404 });
-    } else {
-        res.render('500.jade', { locals: { 
-                  title : 'The Server Encountered an Error'
-                 ,description: ''
-                 ,author: ''
-                 ,analyticssiteid: 'XXXXXXX'
-                 ,error: err 
-                },status: 500 });
-    }
-});
-server.listen( port);
+app.listen(port);
 
 //Setup Socket.IO
-var io = io.listen(server);
+var io = io.listen(app);
+
+io.configure('production',function(){
+	//un-comment 'transports' and 'polling duration' for Heroku, until they implement WebSockets
+	//io.set("transports",["xhr-polling"]);
+	//io.set("polling duration",10);
+	io.set("log level",1);
+});
+
 io.sockets.on('connection', function(socket){
   console.log('Client Connected');
   socket.on('message', function(data){
-    socket.broadcast.emit('server_message',data);
-    socket.emit('server_message',data);
+    socket.broadcast.emit('app_message',data);
+    socket.emit('app_message',data);
   });
   socket.on('disconnect', function(){
     console.log('Client Disconnected.');
@@ -57,7 +85,7 @@ io.sockets.on('connection', function(socket){
 
 /////// ADD ALL YOUR ROUTES HERE  /////////
 
-server.get('/', function(req,res){
+app.get('/', function(req,res){
   res.render('index.jade', {
     locals : { 
               title : 'Your Page Title'
@@ -68,7 +96,7 @@ server.get('/', function(req,res){
   });
 });
 
-server.get('/about', function(req,res){
+app.get('/about', function(req,res){
   res.render('about.jade', {
     locals : { 
               title : 'Your Page Title'
@@ -79,7 +107,7 @@ server.get('/about', function(req,res){
   });
 });
 
-server.get('/contact', function(req,res){
+app.get('/contact', function(req,res){
   res.render('contact.jade', {
     locals : { 
               title : 'Your Page Title'
@@ -92,12 +120,12 @@ server.get('/contact', function(req,res){
 
 
 //A Route for Creating a 500 Error (Useful to keep around)
-server.get('/500', function(req, res){
+app.get('/500', function(req, res){
     throw new Error('This is a 500 Error');
 });
 
 //The 404 Route (ALWAYS Keep this as the last route)
-server.get('/*', function(req, res){
+app.get('/*', function(req, res){
     throw new NotFound;
 });
 
